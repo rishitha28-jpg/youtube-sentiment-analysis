35f0f15aa45e4966aae5a1b36db2b623df846326
Rishitha <sannapureddyrishitha@gmail.com>
2026-03-08 05:00:01 +0530

    switch sentiment analysis to vader

--- app/services/sentiment_service.py
+++ app/services/sentiment_service.py
@@ -1,55 +1,12 @@
-# # from transformers import pipeline
 
-# # # load model once
-# # sentiment_model = pipeline(
-# #     "sentiment-analysis",
-# #     model="distilbert-base-uncased-finetuned-sst-2-english"
-# # )
-
-# # def analyze_sentiments(comments):
-
-# #     results = sentiment_model(comments)
-
-# #     sentiments = []
-
-# #     for r in results:
-# #         sentiments.append({
-# #             "label": r["label"],
-# #             "score": round(r["score"] * 100, 2)
-# #         })
-
-# #     return sentiments
-# from transformers import pipeline
-
-# # load model once
-# sentiment_model = pipeline(
-#     "sentiment-analysis",
-#     model="distilbert-base-uncased-finetuned-sst-2-english",
-#     device=-1
-# )
-
-# def analyze_sentiments(comments):
-
-#     comments = comments[:20]   # limit comments for Render memory
-
-#     results = sentiment_model(comments)
-
-#     sentiments = []
-
-#     for r in results:
-#         sentiments.append({
-#             "label": r["label"],
-#             "score": round(r["score"] * 100, 2)
-#         })
-
-#     return sentiments
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+# load analyzer once
 analyzer = SentimentIntensityAnalyzer()
 
 def analyze_sentiments(comments):
 
-    comments = comments[:20]
+    comments = comments[:20]   # limit comments for safety
 
     sentiments = []
 
