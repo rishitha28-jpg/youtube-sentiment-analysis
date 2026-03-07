65a262a0dfefa5751caa47d1a7deecf050c40d9d
Rishitha <sannapureddyrishitha@gmail.com>
2026-03-08 04:51:19 +0530

    use vader sentiment for render

--- app/services/sentiment_service.py
+++ app/services/sentiment_service.py
@@ -1,12 +1,36 @@
+# # from transformers import pipeline
+
+# # # load model once
+# # sentiment_model = pipeline(
+# #     "sentiment-analysis",
+# #     model="distilbert-base-uncased-finetuned-sst-2-english"
+# # )
+
+# # def analyze_sentiments(comments):
+
+# #     results = sentiment_model(comments)
+
+# #     sentiments = []
+
+# #     for r in results:
+# #         sentiments.append({
+# #             "label": r["label"],
+# #             "score": round(r["score"] * 100, 2)
+# #         })
+
+# #     return sentiments
 # from transformers import pipeline
 
 # # load model once
 # sentiment_model = pipeline(
 #     "sentiment-analysis",
-#     model="distilbert-base-uncased-finetuned-sst-2-english"
+#     model="distilbert-base-uncased-finetuned-sst-2-english",
+#     device=-1
 # )
 
 # def analyze_sentiments(comments):
+
+#     comments = comments[:20]   # limit comments for Render memory
 
 #     results = sentiment_model(comments)
 
@@ -19,27 +43,30 @@
 #         })
 
 #     return sentiments
-from transformers import pipeline
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-# load model once
-sentiment_model = pipeline(
-    "sentiment-analysis",
-    model="distilbert-base-uncased-finetuned-sst-2-english",
-    device=-1
-)
+analyzer = SentimentIntensityAnalyzer()
 
 def analyze_sentiments(comments):
 
-    comments = comments[:20]   # limit comments for Render memory
-
-    results = sentiment_model(comments)
+    comments = comments[:20]
 
     sentiments = []
 
-    for r in results:
+    for comment in comments:
+
+        score = analyzer.polarity_scores(comment)
+
+        if score["compound"] >= 0.05:
+            label = "POSITIVE"
+        elif score["compound"] <= -0.05:
+            label = "NEGATIVE"
+        else:
+            label = "NEUTRAL"
+
         sentiments.append({
-            "label": r["label"],
-            "score": round(r["score"] * 100, 2)
+            "label": label,
+            "score": round(score["compound"] * 100, 2)
         })
 
     return sentiments
